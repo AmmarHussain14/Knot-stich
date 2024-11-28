c321077712ebfe4e826f99070684ee7ba93d816a
Ammar Hussain <88046677+AmmarHussain14@users.noreply.github.com>
2024-11-29 00:01:46 +0530

    [Login} ui first (#1)

---
+++ app/src/main/java/com/example/knotstich/ui/screen/LoginScreen.kt
@@ -0,0 +1,32 @@
+package com.example.knotstich.ui.screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoginScreen(
+    onLoginClick: (String, String) -> Unit,
+    onSignUpClick: () -> Unit
+) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var isLodeing by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
+}
