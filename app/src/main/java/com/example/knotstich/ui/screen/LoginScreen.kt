da5ca0c4c06d900ab9362ef3b06e241f5b6f4e7c
Ammar Hussain <88046677+AmmarHussain14@users.noreply.github.com>
2024-12-02 00:03:41 +0530

    Ammar login (#2)
    * [Login} ui first
    
    * LoginPage1Dec

--- app/src/main/java/com/example/knotstich/ui/screen/LoginScreen.kt
+++ app/src/main/java/com/example/knotstich/ui/screen/LoginScreen.kt
@@ -1,8 +1,20 @@
 package com.example.knotstich.ui.screen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Button
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -10,6 +22,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -19,7 +33,7 @@ fun LoginScreen(
 ) {
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
-    var isLodeing by remember { mutableStateOf(false) }
+    var isLoading by remember { mutableStateOf(false) }
 
     Column(
         modifier = Modifier
@@ -27,6 +41,54 @@ fun LoginScreen(
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Text("Login", style = MaterialTheme.typography.h4, modifier = Modifier.padding(8.dp))
 
+        TextField(
+            value = email,
+            onValueChange = {email = it},
+            label = { Text("Email") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            visualTransformation = PasswordVisualTransformation(),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = {
+                if (email.isNotBlank() && password.isNotBlank()) {
+                    isLoading = true
+                    onLoginClick(email, password)
+                }
+            },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = !isLoading
+        ) {
+            Text("Log In")
+        }
+
+        if (isLoading) {
+            CircularProgressIndicator(modifier = Modifier.padding(16.dp))
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row {
+            Text("Don't have an Account?")
+            Spacer( modifier = Modifier.width(4.dp))
+            Text(
+                text = "Sign up",
+                color = MaterialTheme.colors.primary,
+                modifier = Modifier.clickable { onSignUpClick()}
+            )
+        }
     }
 }
